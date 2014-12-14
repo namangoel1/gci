2353998f2f1ef7d183d44710ec76d976e20a2ac4
Namanyay Goel <mail@namanyayg.com>
2014-12-14 15:19:00 +0530

    Progress on the mailer's tests
    The test sends an actual email now. Next steps would include
    figuring out what data is sent/what is not

--- mailer/tests/testRootPost.js
+++ mailer/tests/testRootPost.js
@@ -3,11 +3,27 @@ var routes = require('../routes/index');
 var mocha = require('mocha');
 
 describe('/ request (post)', function(){
+  this.timeout(100000);
   it('should return successfully', function(done){
+    var testRequest = {};
+
+    testRequest.body =  {
+      to: 'mail@namanyayg.com',
+      subject: 'Test', 
+      text: 'Test',
+    };
+
+    testRequest.flash = sinon.spy();
+
     testResponse = {
-      render: function(){}//can fill this with spies to test
+      render: sinon.spy() //can fill this with spies to test
     }; 
 
-    routes.rootPost({}, testResponse);
+    routes.rootPost(testRequest, testResponse);
+
+    console.log( testRequest.flash.args  );
+
+    if ( testResponse.render.called || testRequest.flash.called )
+      done();
   });
 });
